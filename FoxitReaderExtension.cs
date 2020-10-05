0192f63e202a729d7eade9b3f4ee7f91339cc29a
Devil <devil0153@163.com>
2020-10-05 17:10:15 +0800

    Initial commit

---
+++ FoxitReaderExtension.cs
@@ -0,0 +1,44 @@
+﻿using Ghost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoxitReader
+{
+    public class FoxitReaderExtension : IEmbeddedControlExtension
+    {
+        private FoxitReaderHub foxitReaderHub;
+
+        public bool IsClickThrough => false;
+
+        public HubBase Hub => foxitReaderHub;
+
+        public Control GetControl()
+        {
+            return foxitReaderHub.FoxitCtl;
+        }
+
+        public void Initialize(IExtensionConfig config)
+        {
+            this.foxitReaderHub = new FoxitReaderHub();
+        }
+
+        public void OnConnected()
+        {
+
+        }
+
+        public void OnDisconnect()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ FoxitReaderHub.cs
@@ -0,0 +1,27 @@
+﻿using AxFOXITREADERLib;
+using Ghost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoxitReader
+{
+    public class FoxitReaderHub : HubBase
+    {
+        public AxFoxitCtl FoxitCtl { get; private set; }
+
+        public FoxitReaderHub()
+        {
+            this.FoxitCtl = new AxFoxitCtl();
+            this.FoxitCtl.Dock = DockStyle.Fill;
+        }
+
+        public void OpenFile(string filePath)
+        {
+            this.FoxitCtl.OpenFile(filePath);
+        }
+    }
+}
